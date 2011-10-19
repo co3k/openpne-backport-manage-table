85374736d9bf7fb678da0e80e12a83649b784e90
Kousuke Ebihara <ebihara@php.net>
2011-10-19 17:13:48 +0900

    fixed filter function doesn't work

--- lib.js
+++ lib.js
@@ -27,50 +27,52 @@ $(document).ready(function() {
     $("#issues_table").tablesorter();
 });
 
-$("#filter_button").click(function() {
-    $("#issues_table tbody tr").hide();
+$(document).ready(function() { 
+    $("#filter_button").click(function() {
+        $("#issues_table tbody tr").hide();
 
-    var conditions = {
-        "tracker" : $("#filter_tracker").val(),
-        "status" : $("#filter_status").val(),
-        "priority" : $("#filter_priority").val(),
-        "36" : $("#filter_36").val(),
-        "34" : $("#filter_34").val(),
-        "30" : $("#filter_30").val(),
-    };
+        var conditions = {
+            "tracker" : $("#filter_tracker").val(),
+            "status" : $("#filter_status").val(),
+            "priority" : $("#filter_priority").val(),
+            "36" : $("#filter_36").val(),
+            "34" : $("#filter_34").val(),
+            "30" : $("#filter_30").val(),
+        };
 
-    $("#issues_table tbody tr").filter(function (index) {
-        if (conditions["tracker"] && -1 == jQuery.inArray($("td:nth-child(2)", this).text(), conditions["tracker"])) {
-            console.debug(index+" : tracker");
-            return false;
-        }
+        $("#issues_table tbody tr").filter(function (index) {
+            if (conditions["tracker"] && -1 == jQuery.inArray(jQuery.trim($("td:nth-child(2)", this).text()), conditions["tracker"])) {
+                console.debug(index+" : tracker");
+                return false;
+            }
 
-        if (conditions["status"] && -1 == jQuery.inArray($("td:nth-child(3)", this).text(), conditions["status"])) {
-            console.debug(index+" : status");
-            return false;
-        }
+            if (conditions["status"] && -1 == jQuery.inArray(jQuery.trim($("td:nth-child(3)", this).text()), conditions["status"])) {
+                console.debug(index+" : status");
+                return false;
+            }
 
-        if (conditions["priority"] && -1 == jQuery.inArray($("td:nth-child(4)", this).text(), conditions["priority"])) {
-            console.debug(index+" : priority");
-            return false;
-        }
+            if (conditions["priority"] && -1 == jQuery.inArray(jQuery.trim($("td:nth-child(4)", this).text()), conditions["priority"])) {
+                console.debug(index+" : priority");
+                return false;
+            }
 
-        if (conditions["36"] && -1 == jQuery.inArray($("td:nth-child(7)", this).text(), conditions["36"])) {
-            console.debug(index+" : 36");
-            return false;
-        }
+            if (conditions["36"] && -1 == jQuery.inArray(jQuery.trim($("td:nth-child(7)", this).text()), conditions["36"])) {
+                console.debug(index+" : 36");
+                return false;
+            }
 
-        if (conditions["34"] && -1 == jQuery.inArray($("td:nth-child(8)", this).text(), conditions["34"])) {
-            console.debug(index+" : 34");
-            return false;
-        }
+            if (conditions["34"] && -1 == jQuery.inArray(jQuery.trim($("td:nth-child(8)", this).text()), conditions["34"])) {
+                console.debug(index+" : 34");
+                return false;
+            }
 
-        if (conditions["30"] && -1 == jQuery.inArray($("td:nth-child(9)", this).text(), conditions["30"])) {
-            console.debug(index+" : 30");
-            return false;
-        }
+            if (conditions["30"] && -1 == jQuery.inArray(jQuery.trim($("td:nth-child(9)", this).text()), conditions["30"])) {
+                console.debug(index+" : 30");
+                return false;
+            }
 
-            console.debug(index+" : complete");
-        return true;
-    }).show();
+                console.debug(index+" : complete");
+            return true;
+        }).show();
+    });
 });
